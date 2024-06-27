05d439a77f61b31ecff27975ba0b37d500132a2d
BarbaraCerqueira <1barbararodrigues@gmail.com>
2024-06-27 01:54:13 -0300

    correção

--- iife.js
+++ iife.js
@@ -1,14 +1,12 @@
 const counter = (function () {
     let count = 0;
    
-   
     return function () {
       count += 1;
    
-   
       return count;
     };
-   })();
+})();
 
 counter(); // 1
 counter(); // 2
